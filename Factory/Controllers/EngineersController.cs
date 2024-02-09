f8ce251914927ba0a0f4747b99cf76ba36d67bd1
ChrisRDavila <crossdavila@gmail.com>
2024-02-09 14:13:17 -0800

    add EngineersController and Engineers View/index to display list of engineers from db

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,5 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using ProjectName.Models;
+using Factory.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+    
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers
+                            .OrderBy(engineer => engineer.Name)
+                            .ToList();
+      return View(model);
+    }
+  }
+}
