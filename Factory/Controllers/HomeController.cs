58bb57f8052505cfd86da151f19664c9115cb66d
ChrisRDavila <crossdavila@gmail.com>
2024-02-09 13:22:16 -0800

    add home comtroller and index view for home

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
-
-
-namespace ProjectName.Controllers
+namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/")]
-    // Notice the changes below!
-    public ActionResult File() { return View(); }
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Engineer[] engineers = _db.Engineers.OrderBy(engineers => engineers.Name).ToArray();
+      Machine[] machines = _db.Machines.OrderBy(machines => machines.Model).ToArray();
+      Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+      model.Add("engineers", engineers);
+      model.Add("machines", machines);
+      return View(model);
+    }
+
   }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
 namespace Factory.Models
 {
   public class Machine
